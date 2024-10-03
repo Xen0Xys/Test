b89fb61f0fff61a5694933c15a1472703fa334e5
Xen0Xys <red57101@gmail.com>
2024-10-03 17:21:28 +0200

    :sparkles: Testing API internal connection

--- client/app.vue
+++ client/app.vue
@@ -6,8 +6,11 @@
 <script setup lang="ts">
 import type {VersionEntity} from "../server/src/common/models/entities/version.entity.js";
 
+console.log("API URL :", process.env.API_URL + "/version");
 const result = await useFetch(process.env.API_URL + "/version");
+console.log("Result :", result);
 const version: VersionEntity = result.data.value;
+console.log("Version :", version);
 
 if(version)
     console.log(version.version);
