d836be17f5ea5be7cdc8e9750dec3556fcc9907a
Xen0Xys <red57101@gmail.com>
2024-10-03 17:01:29 +0200

    :sparkles: Testing API internal connection

--- client/app.vue
+++ client/app.vue
@@ -6,7 +6,7 @@
 <script setup lang="ts">
 import type {VersionEntity} from "../server/src/common/models/entities/version.entity.js";
 
-const result = await useFetch("http://nest-app:4000/version");
+const result = await useFetch("https://api.test.coolify.dotslash.fr/version");
 const version: VersionEntity = result.data.value;
 
 if(version)
