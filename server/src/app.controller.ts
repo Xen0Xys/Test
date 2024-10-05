a27b766a22db189f2c9a09863ce2d6c33cce84f5
Xen0Xys <red57101@gmail.com>
2024-10-05 14:10:47 +0200

    :sparkles: Add redis caching

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,6 +1,7 @@
-import {Controller, Get, HttpStatus} from "@nestjs/common";
+import {Controller, Get, HttpStatus, UseInterceptors} from "@nestjs/common";
 import {ApiResponse, ApiTags} from "@nestjs/swagger";
 import {VersionEntity} from "./common/models/entities/version.entity";
+import {CacheInterceptor} from "@nestjs/cache-manager";
 
 @Controller()
 @ApiTags("Misc")
@@ -9,6 +10,7 @@ export class AppController{
     }
 
     @Get("version")
+    @UseInterceptors(CacheInterceptor)
     @ApiResponse({status: HttpStatus.OK, description: "Returns the version of the application", type: VersionEntity})
     getVersion(){
         return {version: process.env.npm_package_version};
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import {FilesModule} from "./modules/files/files.module";
 import {UsersModule} from "./modules/users/users.module";
+import * as redisStore from "cache-manager-redis-store";
 import {AuthModule} from "./modules/auth/auth.module";
 import {CronModule} from "./modules/cron/cron.module";
 import {MailerModule} from "@nestjs-modules/mailer";
 import {ThrottlerModule} from "@nestjs/throttler";
+import {CacheModule} from "@nestjs/cache-manager";
 import {ScheduleModule} from "@nestjs/schedule";
+import type {RedisClientOptions} from "redis";
 import {ConfigModule} from "@nestjs/config";
 import {Module} from "@nestjs/common";
 import * as dotenv from "dotenv";
@@ -17,6 +20,17 @@ dotenv.config();
     imports: [
         ConfigModule.forRoot({isGlobal: true}),
         ScheduleModule.forRoot(),
+        process.env.REDIS_URL === "" ? (() => {
+            console.log("Using in-memory cache");
+            return CacheModule.register({isGlobal: true});
+        })() : (() => {
+            console.log("Using Redis cache");
+            return CacheModule.register<RedisClientOptions>({
+                isGlobal: true,
+                store: redisStore,
+                url: process.env.REDIS_URL,
+            });
+        })(),
         ThrottlerModule.forRoot([{
             ttl: 60000,
             limit: 50,
