cb6ef91791c281b9b16e8c5cef9b027dda845d9e
Xen0Xys <red57101@gmail.com>
2024-10-05 14:54:12 +0200

    :ambulance: Fix docker caching

--- client/app.vue
+++ client/app.vue
@@ -7,11 +7,8 @@
 import type {VersionEntity} from "../server/src/common/models/entities/version.entity.js";
 const runtimeConfig = useRuntimeConfig();
 
-console.log("API URL :", runtimeConfig.public.apiUrl + "/version");
 const result = await useFetch(runtimeConfig.public.apiUrl + "/version");
-console.log("Result :", result);
 const version: VersionEntity = result.data.value;
-console.log("Version :", version);
 
 if(version)
     console.log(version.version);
